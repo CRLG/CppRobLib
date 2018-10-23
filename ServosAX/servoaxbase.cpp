6c1ee138f2ebc90d78e3a179024e1fe45ac7fe17
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2018-10-23 11:09:08 +0200

    #1 : Version initiale du module de pilotage des servomoteurs Dynamixel AX
    Classe virtuelle pure.
    Méthodes dépendante du hardware à réimplémenter sur la cible dans une classe fille.

---
+++ ServosAX/servoaxbase.cpp
@@ -0,0 +1,318 @@
+#include "servoaxbase.h"
+
+ServoAXBase::ServoAXBase()
+{
+}
+
+ServoAXBase::~ServoAXBase()
+{
+}
+
+// ____________________________________________________________
+bool ServoAXBase::isPresent(unsigned char id)
+{
+    unsigned char val;
+    tAxErr err = read8bitsRegister(id, AX_REG_VERSION, &val);
+    return (err == AX_OK);
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::setPosition(unsigned char id, unsigned short position)
+{
+    return write16bitsRegister(id, AX_REG_GOAL_POSITION, position);
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::setSpeed(unsigned char id, unsigned short speed)
+{
+    return write16bitsRegister(id, AX_REG_GOAL_SPEED, speed);
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::setPositionSpeed(unsigned char id, unsigned short position, unsigned short speed)
+{
+    unsigned char data[5];
+    data[0] = AX_REG_GOAL_POSITION;
+    data[1] = position;
+    data[2] = (position>>8);
+    data[3] = speed;
+    data[4] = (speed>>8);
+    return createWriteDataPacket(id, data, 5);
+}
+
+// ____________________________________________________________
+unsigned short ServoAXBase::getPosition(unsigned char id, tAxErr *err_status)
+{
+    unsigned short position=0;
+    tAxErr err;
+    err = read16bitsRegister(id, AX_REG_PRESENT_POSITION, &position);
+    if (err_status != nullptr) *err_status = err;
+    return position;
+}
+
+// ____________________________________________________________
+bool ServoAXBase::isMoving(unsigned char id, tAxErr *err_status)
+{
+    unsigned char moving=0;
+    tAxErr err;
+    err = read8bitsRegister(id, AX_REG_MOVING, &moving);
+    if (err_status != nullptr) *err_status = err;
+    return moving;
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::changeID(unsigned char old_id, unsigned char new_id)
+{
+    if (isPresent(old_id) == false) return AX_NOT_FOUND;
+
+    tAxErr err;
+    err = write8bitsRegister(old_id, AX_REG_ID, new_id);
+    if (err != AX_OK) return AX_NOT_FOUND;
+
+    return AX_OK;
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::enableTorque(unsigned char id, bool on_off)
+{
+    return write8bitsRegister(id, AX_REG_TORQUE_ENABLE, on_off);
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::setLimitPositionMin(unsigned char id, unsigned short pos)
+{
+    return write16bitsRegister(id, AX_REG_CW_ANGLE_LIMIT, pos);
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::setLimitPositionMax(unsigned char id, unsigned short pos)
+{
+    return write16bitsRegister(id, AX_REG_CCW_ANGLE_LIMIT, pos);
+}
+
+// ____________________________________________________________
+tAxErr ServoAXBase::setMode(unsigned char id, unsigned char mode)
+{
+    tAxErr err;
+    unsigned short cw_limit, ccw_limit;
+
+    switch (mode)
+    {
+        case AX_MODE_ROTATIONAL:
+            cw_limit = 0;
+            ccw_limit = 0x3FF;
+        break;
+
+        case AX_MODE_FREE_RUNNING:
+            cw_limit = 0;
+            ccw_limit = 0;
+        break;
+
+        default :
+            return AX_INVALID_VALUE;
+    }
+    err = setSpeed(id, 0);  // ensure motor won't move after mode is changing
+    if (err != AX_OK) return err;
+    err = setLimitPositionMin(id, cw_limit);
+    if (err != AX_OK) return err;
+    err = setLimitPositionMax(id, ccw_limit);
+    if (err != AX_OK) return err;
+    return AX_OK;
+}
+
+// ____________________________________________________________
+/*! \brief Write a value in a 16 bits register.
+ *
+ * \param id : the identifer of the servo
+ * \param reg_addr : the start address of the register
+ * \param value : the value to write
+ * \return error code
+ */
+tAxErr ServoAXBase::write16bitsRegister(unsigned char id, unsigned char reg_addr, unsigned short value)
+{
+    unsigned char data[3];
+    data[0] = reg_addr;
+    data[1] = value;
+    data[2] = (value>>8);
+    return createWriteDataPacket(id, data, 3);
+}
+
+// ____________________________________________________________
+/*! \brief Read the value in a 8 bits register.
+ *
+ * \param id : the identifer of the servo
+ * \param reg_addr : the start address of the register
+ * \param value : the return value read in the register
+ * \return error code
+ */
+tAxErr ServoAXBase::read8bitsRegister(unsigned char id, unsigned char reg_addr, unsigned char *value, unsigned char *err_status)
+{
+    tAxErr err;
+
+    err = createReadDataPacket(id, reg_addr, 1);
+    if (err != AX_OK) return err;
+
+    err = read(m_packet, 7);
+    if (err != AX_OK) return err;
+
+    if (!isPacketValid(m_packet)) return AX_INVALID_PACKET;
+
+    if (err_status) *err_status = m_packet[4];
+    if (value) *value = m_packet[5];
+
+    return AX_OK;
+}
+
+// ____________________________________________________________
+/*! \brief Read the value in a 16 bits register.
+ *
+ * \param id : the identifer of the servo
+ * \param reg_addr : the start address of the register
+ * \param value : the return value read in the register
+ * \return error code
+ */
+tAxErr ServoAXBase::read16bitsRegister(unsigned char id, unsigned char reg_addr, unsigned short *value, unsigned char *err_status)
+{
+    tAxErr err;
+
+    err = createReadDataPacket(id, reg_addr, 2);
+    if (err != AX_OK) return err;
+
+    err = read(m_packet, 8);
+    if (err != AX_OK) return err;
+
+    if (!isPacketValid(m_packet)) return AX_INVALID_PACKET;
+
+    if (err_status) *err_status = m_packet[4];
+    if (value) *value = ((unsigned short)m_packet[6]<<8) + m_packet[5];
+
+    return AX_OK;
+}
+
+// ____________________________________________________________
+/*! \brief Write a value in a 8 bits register.
+ *
+ * \param id : the identifer of the servo
+ * \param reg_addr : the start address of the register
+ * \param value : the value to write
+ * \return error code
+ */
+tAxErr ServoAXBase::write8bitsRegister(unsigned char id, unsigned char reg_addr, unsigned char value)
+{
+    unsigned char data[2];
+    data[0] = reg_addr;
+    data[1] = value;
+    return createWriteDataPacket(id, data, 2);
+}
+
+// ____________________________________________________________
+/*! \brief Create a Write Data packet.
+ *
+ * \param id : the identifer of the servo
+ * \param parameters : the array of useful parameters values
+ * \param size : the number of parameters
+// * \param packet : the destination buffer
+ * \return error code
+ * \remarks : the buffer is supposed to be allocated to receive all data
+ *      Instruction Packet : 0XFF 0XFF ID LENGTH INSTRUCTION PARAMETER1 ...PARAMETER_N CHECKSUM
+ */
+tAxErr ServoAXBase::createWriteDataPacket(unsigned char id, unsigned char *parameters, unsigned char size, bool send_packet)
+{
+    m_packet[0] = 0xFF;
+    m_packet[1] = 0xFF;
+    m_packet[2] = id;
+    m_packet[3] = size + 2;
+    m_packet[4] = AX_INSTRUCTION_WRITE_DATA;
+    unsigned int i;
+    for (i=0; i<size; i++) {
+        m_packet[i+5] = parameters[i];
+    }
+    m_packet[i+5] = getChecksum(m_packet);
+
+    if (send_packet) {
+        return _write(m_packet, i+6);
+    }
+    return AX_OK;
+}
+
+// ____________________________________________________________
+/*! \brief Create a Read Data packet.
+ *
+ * \param id : the identifer of the servo
+ * \param start_addr : the first address to read
+ * \param length: the number of bytes to read
+ * \return error code
+ * \remarks :
+ *      Read Packet : 0XFF 0XFF ID 0x04 0x02 start_addr length CHECKSUM
+ */
+tAxErr ServoAXBase::createReadDataPacket(unsigned char id, unsigned char start_addr, unsigned char length)
+{
+    m_packet[0] = 0xFF;
+    m_packet[1] = 0xFF;
+    m_packet[2] = id;
+    m_packet[3] = 4; // packet length
+    m_packet[4] = AX_INSTRUCTION_READ_DATA;
+    m_packet[5] = start_addr;
+    m_packet[6] = length;
+    m_packet[7] = getChecksum(m_packet);
+
+    flushSerialInput();  // ensure no data is pending in uart input buffer
+    return _write(m_packet, 8);
+}
+
+// ____________________________________________________________
+/*! \brief Write packet.
+ *
+ * \param buff_data: the buffer data to be written
+ * \param size: the size of the buffer
+ * \return error code
+ */
+tAxErr ServoAXBase::_write(unsigned char *buff_data, unsigned char size)
+{
+    tAxErr err = AX_OK;
+    setTxEnable(true);
+    delay_us(1);
+    err = write(buff_data, size);
+    waitTransmitComplete();  // ensure transmission is complete before enable line is reset
+    delay_us(1);
+    setTxEnable(false);
+    return err;
+}
+
+// ____________________________________________________________
+/*! \brief Compute checksum from the packet.
+ *
+ * \param packet : the packet to compute
+ * \return the checksum
+ * \remarks :
+ *      Instruction Packet : 0XFF 0XFF ID LENGTH INSTRUCTION PARAMETER1 ...PARAMETER_N CHECKSUM
+ *          Check Sum = ~ (ID + Length + Instruction + Parameter1 + ... Parameter N) for write packet
+ *          Check Sum = ~ (ID + Length + Error + Parameter1 + ... Parameter N) for status packet
+ */
+unsigned char ServoAXBase::getChecksum(unsigned char *packet)
+{
+    unsigned char sum=0;
+    for (int i=2; i<=2+packet[3]; i++) {
+        sum+= packet[i];
+    }
+    return ~sum;
+}
+
+// ____________________________________________________________
+/*! \brief Check if packet is valid.
+ *   A valid packet is :
+ *      first two bytes are 0xff
+ *      checksum is OK
+ *
+ * \param packet : the packet to compute
+ * \return the checksum
+ */
+//!
+bool ServoAXBase::isPacketValid(unsigned char *packet)
+{
+    if (packet[0] != 0xFF) return false;
+    if (packet[1] != 0xFF) return false;
+    unsigned char cs_index = 3 + packet[3];
+    if (getChecksum(packet) != packet[cs_index]) return false;
+    return true;
+}
---
+++ ServosAX/servoaxbase.h
@@ -0,0 +1,189 @@
+#ifndef _SERVOAX_BASE_H
+#define _SERVOAX_BASE_H
+
+// ====================================================
+//                  AX12 REGISTER DEFINITION
+// ====================================================
+// EEPROM AREA
+#define AX_REG_MODEL_NUMBER_L           0
+#define AX_REG_MODEL_NUMBER             AX_REG_MODEL_NUMBER_L
+#define AX_REG_MODEL_NUMBER_H           1
+#define AX_REG_VERSION                  2
+#define AX_REG_ID                       3
+#define AX_REG_BAUD_RATE                4
+#define AX_REG_RETURN_DELAY_TIME        5
+#define AX_REG_CW_ANGLE_LIMIT_L         6
+#define AX_REG_CW_ANGLE_LIMIT           AX_REG_CW_ANGLE_LIMIT_L
+#define AX_REG_CW_ANGLE_LIMIT_H         7
+#define AX_REG_CCW_ANGLE_LIMIT_L        8
+#define AX_REG_CCW_ANGLE_LIMIT          AX_REG_CCW_ANGLE_LIMIT_L
+#define AX_REG_CCW_ANGLE_LIMIT_H        9
+#define AX_REG_SYSTEM_DATA2             10
+#define AX_REG_LIMIT_TEMPERATURE        11
+#define AX_REG_DOWN_LIMIT_VOLTAGE       12
+#define AX_REG_UP_LIMIT_VOLTAGE         13
+#define AX_REG_MAX_TORQUE_L             14
+#define AX_REG_MAX_TORQUE               AX_REG_MAX_TORQUE_L
+#define AX_REG_MAX_TORQUE_H             15
+#define AX_REG_RETURN_LEVEL             16
+#define AX_REG_ALARM_LED                17
+#define AX_REG_ALARM_SHUTDOWN           18
+#define AX_REG_OPERATING_MODE           19
+#define AX_REG_DOWN_CALIBRATION_L       20
+#define AX_REG_DOWN_CALIBRATION         AX_REG_DOWN_CALIBRATION_L
+#define AX_REG_DOWN_CALIBRATION_H       21
+#define AX_REG_UP_CALIBRATION_L         22
+#define AX_REG_UP_CALIBRATION           AX_REG_UP_CALIBRATION_L
+#define AX_REG_UP_CALIBRATION_H         23
+// RAM AREA
+#define AX_REG_TORQUE_ENABLE            24
+#define AX_REG_LED                      25
+#define AX_REG_CW_COMPLIANCE_MARGIN     26
+#define AX_REG_CCW_COMPLIANCE_MARGIN    27
+#define AX_REG_CW_COMPLIANCE_SLOPE      28
+#define AX_REG_CCW_COMPLIANCE_SLOPE     29
+#define AX_REG_GOAL_POSITION_L          30
+#define AX_REG_GOAL_POSITION            AX_REG_GOAL_POSITION_L
+#define AX_REG_GOAL_POSITION_H          31
+#define AX_REG_GOAL_SPEED_L             32
+#define AX_REG_GOAL_SPEED               AX_REG_GOAL_SPEED_L
+#define AX_REG_GOAL_SPEED_H             33
+#define AX_REG_TORQUE_LIMIT_L           34
+#define AX_REG_TORQUE_LIMIT             AX_REG_TORQUE_LIMIT_L
+#define AX_REG_TORQUE_LIMIT_H           35
+#define AX_REG_PRESENT_POSITION_L       36
+#define AX_REG_PRESENT_POSITION         AX_REG_PRESENT_POSITION_L
+#define AX_REG_PRESENT_POSITION_H       37
+#define AX_REG_PRESENT_SPEED_L          38
+#define AX_REG_PRESENT_SPEED            AX_REG_PRESENT_SPEED_L
+#define AX_REG_PRESENT_SPEED_H          39
+#define AX_REG_PRESENT_LOAD_L           40
+#define AX_REG_PRESENT_LOAD             AX_REG_PRESENT_LOAD_L
+#define AX_REG_PRESENT_LOAD_H           41
+#define AX_REG_PRESENT_VOLTAGE          42
+#define AX_REG_PRESENT_TEMPERATURE      43
+#define AX_REG_REGISTERED_INSTRUCTION   44
+#define AX_REG_PAUSE_TIME               45
+#define AX_REG_MOVING                   46
+#define AX_REG_LOCK                     47
+#define AX_REG_PUNCH_L                  48
+#define AX_REG_PUNCH                    AX_REG_PUNCH_L
+#define AX_REG_PUNCH_H                  49
+
+// ====================================================
+//            AX12 INSTRUCTIONS DEFINITION
+// ====================================================
+#define AX_INSTRUCTION_PING             0x1
+#define AX_INSTRUCTION_READ_DATA        0x2
+#define AX_INSTRUCTION_WRITE_DATA       0x3
+#define AX_INSTRUCTION_REG_WRITE        0x4
+#define AX_INSTRUCTION_ACTION           0x5
+#define AX_INSTRUCTION_RESET            0x6
+#define AX_INSTRUCTION_RESET_SYNC_WRITE 0x83
+
+// ====================================================
+//                    AX STATUS CODE
+// ====================================================
+// Bitfield
+#define AX_STATUS_ERROR_INPUT_VOLTAGE   0x01
+#define AX_STATUS_ERROR_ANGLE_LIMIT     0x02
+#define AX_STATUS_ERROR_TEMPERATURE     0x04
+#define AX_STATUS_ERROR_RANGE           0x08
+#define AX_STATUS_ERROR_CHECKSUM        0x10
+#define AX_STATUS_ERROR_TORQUE          0x20
+#define AX_STATUS_ERROR_INSTRUCTION     0x40
+
+// ====================================================
+//                    AX BAUD RATE
+// ====================================================
+#define AX_BAUD_1MBPS       0x01
+#define AX_BAUD_500KBPS     0x03
+#define AX_BAUD_400KBPS     0x04
+#define AX_BAUD_250KBPS     0x07
+#define AX_BAUD_200KBPS     0x09
+#define AX_BAUD_115200BPS   0x10
+#define AX_BAUD_57600BPS    0x22
+#define AX_BAUD_19200BPS    0x67
+#define AX_BAUD_9600BPS     0xCF
+
+
+// ====================================================
+//               INTERNAL DRIVER ERROR CODE
+// ====================================================
+typedef enum {
+    AX_OK = 0,
+    AX_TIMEOUT,
+    AX_INVALID_PACKET,
+    AX_WRITE_ERROR,
+    AX_NOT_FOUND,
+    AX_INVALID_VALUE
+}tAxErr;
+
+// ====================================================
+//               RUNNING MODE
+// ====================================================
+#define AX_MODE_ROTATIONAL      0
+#define AX_MODE_FREE_RUNNING    1
+
+// ====================================================
+//
+// ====================================================
+#define BROADCAST_ID 0xFE
+
+
+// ====================================================
+//          ABSTRACT CLASS FOR AX SERVOMOTORS
+// ====================================================
+class ServoAXBase
+{
+public:
+    ServoAXBase();
+    virtual ~ServoAXBase();
+
+    // pure virtual methods for hardware abstraction.
+    // to be implemented on specific hardware.
+    virtual tAxErr read(unsigned char *buff_data, unsigned char size) = 0;
+    virtual tAxErr write(unsigned char *buff_data, unsigned char size) = 0;
+    virtual tAxErr flushSerialInput() = 0;
+    virtual tAxErr waitTransmitComplete() = 0;
+    virtual tAxErr setTxEnable(bool state) = 0;
+    virtual void delay_us(unsigned long delay) = 0;
+
+    // High level API
+    virtual bool isPresent(unsigned char id);
+    virtual tAxErr setPosition(unsigned char id, unsigned short position);
+    virtual unsigned short getPosition(unsigned char id, tAxErr *err_status=nullptr);
+    virtual tAxErr setSpeed(unsigned char id, unsigned short speed);
+    virtual tAxErr setPositionSpeed(unsigned char id, unsigned short position, unsigned short speed);
+    virtual bool isMoving(unsigned char id, tAxErr *err_status=nullptr);
+    virtual tAxErr changeID(unsigned char old_id, unsigned char new_id);
+    virtual tAxErr enableTorque(unsigned char id, bool on_off=true);
+    virtual tAxErr setMode(unsigned char id, unsigned char mode);
+    virtual tAxErr setLimitPositionMin(unsigned char id, unsigned short pos);
+    virtual tAxErr setLimitPositionMax(unsigned char id, unsigned short pos);
+
+    // Medium level API
+    tAxErr read8bitsRegister(unsigned char id, unsigned char reg_addr, unsigned char *value, unsigned char *err_status=nullptr);
+    tAxErr read16bitsRegister(unsigned char id, unsigned char reg_addr, unsigned short *value, unsigned char *err_status=nullptr);
+    tAxErr write8bitsRegister(unsigned char id, unsigned char reg_addr, unsigned char value);
+    tAxErr write16bitsRegister(unsigned char id, unsigned char reg_addr, unsigned short value);
+
+private:
+    //! Create an instruction packet based on useful parameters
+    tAxErr createWriteDataPacket(unsigned char id, unsigned char *parameters, unsigned char size, bool send_packet=true);
+    //! Create a read packet based on request parameters
+    tAxErr createReadDataPacket(unsigned char id, unsigned char start_addr, unsigned char length);
+
+    //! Compute checksum from the packet
+    unsigned char getChecksum(unsigned char *packet);
+
+    //! Check if packet is valid
+    bool isPacketValid(unsigned char *packet);
+
+    tAxErr _write(unsigned char *buff_data, unsigned char size);
+
+    //! Communication is half-duplex so the same packet buffer can be used for both transmit or receive communication without conflict
+    unsigned char m_packet[32];
+};
+
+#endif // _SERVOAX_BASE_H
