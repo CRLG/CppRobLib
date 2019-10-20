7cc301c5e05d0c67d5afd1a3212c5709d260466c
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2019-10-20 22:36:50 +0200

    #9 [PowerElectrobot] Version initiale du driver bas niveau de communication avec la carte
    Classe abstraite pure générique.
    Certaines fonctions de lien avec le hardware sont à ré-implémenter par héritage sur la plateforme cible.

---
+++ PowerElectrobot/dspicpowerelectrobotbase.cpp
@@ -0,0 +1,227 @@
+#include "dspicpowerelectrobotbase.h"
+
+dsPicPowerElectrobotBase::dsPicPowerElectrobotBase()
+    : m_compteurErrCom(0)
+{
+
+}
+
+dsPicPowerElectrobotBase::~dsPicPowerElectrobotBase()
+{
+
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::init(unsigned char _8bits_i2c_addr)
+{
+    m_address = _8bits_i2c_addr;
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::periodicCall()
+{
+    readRegisters();
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::readRegisters()
+{
+    unsigned char checksum=0;
+    unsigned char i;
+    unsigned char buff[16];
+
+    buff[8] = 0xFF; // Pour être certain de ne pas conserver un bon checksum du coup d'avant
+    readI2C(buff, 9);
+    // Calcul le checksum à partir des données reçues
+    for (i=0; i<8; i++) {
+      checksum+= buff[i];
+    }
+
+    // Ne prend en compte les données reçues que si le checksum est bon
+    if (checksum == buff[8]) {
+        unsigned short val;
+        val = (((unsigned short)buff[0])<<8) + buff[1];
+        m_raw_battery_voltage = val;
+        m_battery_voltage = RegisterToBatteryVoltage(val);
+
+        val = (((unsigned short)buff[2])<<8) + buff[3];
+        m_raw_global_current = val;
+        m_global_current = RegisterToGlobalCurrent(val);
+
+        val = (((unsigned short)buff[4])<<8) + buff[5];
+        m_raw_current_out1 = val;
+        m_current_out1 = RegisterToCurrentOut1(val);
+
+        val = (((unsigned short)buff[6])<<8) + buff[7];
+        m_raw_current_out2 = val;
+        m_current_out2 = RegisterToCurrentOut2(val);
+    }
+    else {
+      m_compteurErrCom++;
+    }
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::writeRegister(unsigned char reg, unsigned char val)
+{
+    unsigned char buff[4];
+    buff[0] = 3;
+    buff[1] = reg;
+    buff[2] = val;
+    buff[3] =  buff[0] + buff[1] + buff[2];
+    writeI2C(buff, 4);
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::writeWordRegister(unsigned char reg, unsigned short val)
+{
+    unsigned char buff[5];
+    buff[0] = 4;
+    buff[1] = reg;
+    buff[2] = val>>8;   // MSB
+    buff[3] = val;      // LSB
+    buff[4] =  buff[0] + buff[1] + buff[2] + buff[3];
+    writeI2C(buff, 5);
+}
+
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::RegisterToBatteryVoltage(unsigned short val)
+{
+    return (float)val / 1000.0f;    // [mV] -> [V]
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::RegisterToGlobalCurrent(unsigned short val)
+{
+    return (float)val / 1000.0f;    // [mA] -> [A]
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::RegisterToCurrentOut1(unsigned short val)
+{
+    return (float)val / 1000.0f;    // [mA] -> [A]
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::RegisterToCurrentOut2(unsigned short val)
+{
+    return (float)val / 1000.0f;
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::getBatteryVoltage()
+{
+    return m_battery_voltage;
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::getGlobalCurrent()
+{
+    return m_global_current;
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::getCurrentOut1()
+{
+    return m_current_out1;
+}
+
+// ______________________________________________
+float dsPicPowerElectrobotBase::getCurrentOut2()
+{
+    return m_current_out2;
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::setOutput(tSwitchOutput output, bool state)
+{
+    if (output > OUTPUT_STOR8) return;
+    switch(output) {
+        case OUTPUT_STOR1 : writeRegister(REG_STOR_1, state); break;
+        case OUTPUT_STOR2 : writeRegister(REG_STOR_2, state); break;
+        case OUTPUT_STOR3 : writeRegister(REG_STOR_3, state); break;
+        case OUTPUT_STOR4 : writeRegister(REG_STOR_4, state); break;
+        case OUTPUT_STOR5 : writeRegister(REG_STOR_5, state); break;
+        case OUTPUT_STOR6 : writeRegister(REG_STOR_6, state); break;
+        case OUTPUT_STOR7 : writeRegister(REG_STOR_7, state); break;
+        case OUTPUT_STOR8 : writeRegister(REG_STOR_8, state); break;
+        default : break;
+    }
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::setAllOutputs(bool state)
+{
+    for (unsigned int i=OUTPUT_STOR1; i<=OUTPUT_STOR8; i++) {
+        setOutput((tSwitchOutput)i, state);
+    }
+}
+
+// ______________________________________________
+unsigned long dsPicPowerElectrobotBase::getCountError()
+{
+    return m_compteurErrCom;
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::unlockWriteEEPROM()
+{
+    writeRegister(REG_EEPROM_WRITE_UNPROTECT, EEPROM_WRITE_UNPROTECT_CODE);
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::resetFactoryEEPROM()
+{
+    writeRegister(REG_EEPROM_RESET_FACTORY, EEPROM_RESET_FACTORY_CODE);
+}
+
+// ______________________________________________
+void dsPicPowerElectrobotBase::changeI2CAddress(unsigned char _8bits_i2c_addr)
+{
+    writeRegister(REG_I2C_8BITS_ADDRESS, _8bits_i2c_addr);
+}
+
+
+// ==============================================
+//                      CALIBRATION
+// ==============================================
+
+void dsPicPowerElectrobotBase::setCalibrationBatteryVoltagePoint1(unsigned short voltage_mV)
+{
+    writeWordRegister(REG_CALIB_BATTERY_VOLTAGE_PHYS_POINT_1_H, voltage_mV);
+}
+void dsPicPowerElectrobotBase::setCalibrationBatteryVoltagePoint2(unsigned short voltage_mV)
+{
+    writeWordRegister(REG_CALIB_BATTERY_VOLTAGE_PHYS_POINT_2_H, voltage_mV);
+}
+
+void dsPicPowerElectrobotBase::setCalibrationGlobalCurrentPoint1(unsigned short current_mA)
+{
+    writeWordRegister(REG_CALIB_GLOBAL_CURRENT_PHYS_POINT_1_H, current_mA);
+}
+
+void dsPicPowerElectrobotBase::setCalibrationGlobalCurrentPoint2(unsigned short current_mA)
+{
+    writeWordRegister(REG_CALIB_GLOBAL_CURRENT_PHYS_POINT_2_H, current_mA);
+}
+
+void dsPicPowerElectrobotBase::setCalibrationCurrentOut1Point1(unsigned short current_mA)
+{
+    writeWordRegister(REG_CALIB_CURRENT_OUT1_PHYS_POINT_1_H, current_mA);
+}
+
+void dsPicPowerElectrobotBase::setCalibrationCurrentOut1Point2(unsigned short current_mA)
+{
+    writeWordRegister(REG_CALIB_CURRENT_OUT1_PHYS_POINT_2_H, current_mA);
+}
+
+void dsPicPowerElectrobotBase::setCalibrationCurrentOut2Point1(unsigned short current_mA)
+{
+    writeWordRegister(REG_CALIB_CURRENT_OUT2_PHYS_POINT_1_H, current_mA);
+}
+
+void dsPicPowerElectrobotBase::setCalibrationCurrentOut2Point2(unsigned short current_mA)
+{
+    writeWordRegister(REG_CALIB_CURRENT_OUT2_PHYS_POINT_2_H, current_mA);
+}
---
+++ PowerElectrobot/dspicpowerelectrobotbase.h
@@ -0,0 +1,136 @@
+#ifndef DSPIC_POWER_ELECTROBOT_BASE_H
+#define DSPIC_POWER_ELECTROBOT_BASE_H
+
+class dsPicPowerElectrobotBase
+{
+    // Enum commun avec le SW dsPIC
+    typedef enum {
+        // Read Only registers
+        REG_VERSION_SOFT_MAJ = 0,
+        REG_VERSION_SOFT_MIN,
+        REG_PTR_REG_LECTURE_I2C,
+        REG_NBRE_REGISTRES_LECTURE_I2C,
+        REG_EANA_VBAT_H,
+        REG_EANA_VBAT_L,
+        REG_EANA_MEAS_CURR_VBAT_H,
+        REG_EANA_MEAS_CURR_VBAT_L,
+        REG_EANA_MEAS_CURR_1_H,
+        REG_EANA_MEAS_CURR_1_L,
+        REG_EANA_MEAS_CURR_2_H,
+        REG_EANA_MEAS_CURR_2_L,
+        // Read write registers
+        REG_STOR_1,
+        REG_STOR_2,
+        REG_STOR_3,
+        REG_STOR_4,
+        REG_STOR_5,
+        REG_STOR_6,
+        REG_STOR_7,
+        REG_STOR_8,
+        REG_STOR_PGED,
+        REG_STOR_PGEC,
+        REG_CALIB_BATTERY_VOLTAGE_PHYS_POINT_1_H,
+        REG_CALIB_BATTERY_VOLTAGE_PHYS_POINT_1_L,
+        REG_CALIB_BATTERY_VOLTAGE_PHYS_POINT_2_H,
+        REG_CALIB_BATTERY_VOLTAGE_PHYS_POINT_2_L,
+        REG_CALIB_GLOBAL_CURRENT_PHYS_POINT_1_H,
+        REG_CALIB_GLOBAL_CURRENT_PHYS_POINT_1_L,
+        REG_CALIB_GLOBAL_CURRENT_PHYS_POINT_2_H,
+        REG_CALIB_GLOBAL_CURRENT_PHYS_POINT_2_L,
+        REG_CALIB_CURRENT_OUT1_PHYS_POINT_1_H,
+        REG_CALIB_CURRENT_OUT1_PHYS_POINT_1_L,
+        REG_CALIB_CURRENT_OUT1_PHYS_POINT_2_H,
+        REG_CALIB_CURRENT_OUT1_PHYS_POINT_2_L,
+        REG_CALIB_CURRENT_OUT2_PHYS_POINT_1_H,
+        REG_CALIB_CURRENT_OUT2_PHYS_POINT_1_L,
+        REG_CALIB_CURRENT_OUT2_PHYS_POINT_2_H,
+        REG_CALIB_CURRENT_OUT2_PHYS_POINT_2_L,
+
+        REG_I2C_8BITS_ADDRESS,
+        REG_EEPROM_WRITE_UNPROTECT,
+        REG_EEPROM_RESET_FACTORY,
+        // _____________________________
+        MAX_REGISTRES_NUMBER
+
+    }T_REG_ADDRESS;
+    #define EEPROM_WRITE_UNPROTECT_CODE (0x5A)
+    #define EEPROM_RESET_FACTORY_CODE (0x69)
+
+public:
+    dsPicPowerElectrobotBase();
+    virtual ~dsPicPowerElectrobotBase();
+
+    virtual void writeI2C(unsigned char *buff, unsigned short size) = 0;
+    virtual void readI2C(unsigned char *dest_buff, unsigned short size) = 0;
+
+    void init(unsigned char _8bits_i2c_addr);
+
+    void periodicCall();
+
+    float getBatteryVoltage();
+    float getGlobalCurrent();
+    float getCurrentOut1();
+    float getCurrentOut2();
+    unsigned long getCountError();
+
+    void unlockWriteEEPROM();
+    void resetFactoryEEPROM();
+    void changeI2CAddress(unsigned char _8bits_i2c_addr);
+
+    void setCalibrationBatteryVoltagePoint1(unsigned short voltage_mV);
+    void setCalibrationBatteryVoltagePoint2(unsigned short voltage_mV);
+    void setCalibrationGlobalCurrentPoint1(unsigned short current_mA);
+    void setCalibrationGlobalCurrentPoint2(unsigned short current_mA);
+    void setCalibrationCurrentOut1Point1(unsigned short current_mA);
+    void setCalibrationCurrentOut1Point2(unsigned short current_mA);
+    void setCalibrationCurrentOut2Point1(unsigned short current_mA);
+    void setCalibrationCurrentOut2Point2(unsigned short current_mA);
+
+    typedef enum {
+        OUTPUT_STOR1 = 0,
+        OUTPUT_STOR2,
+        OUTPUT_STOR3,
+        OUTPUT_STOR4,
+        OUTPUT_STOR5,
+        OUTPUT_STOR6,
+        OUTPUT_STOR7,
+        OUTPUT_STOR8
+    }tSwitchOutput;
+#define OUTPUT_J11 OUTPUT_STOR1
+#define OUTPUT_J13 OUTPUT_STOR2
+#define OUTPUT_J16 OUTPUT_STOR3
+#define OUTPUT_J18 OUTPUT_STOR4
+#define OUTPUT_J12 OUTPUT_STOR5
+#define OUTPUT_J14 OUTPUT_STOR6
+#define OUTPUT_J17 OUTPUT_STOR7
+#define OUTPUT_J19 OUTPUT_STOR8
+
+    void setOutput(tSwitchOutput output, bool state);
+    void setAllOutputs(bool state);
+
+    unsigned short m_raw_battery_voltage;
+    unsigned short m_raw_global_current;
+    unsigned short m_raw_current_out1;
+    unsigned short m_raw_current_out2;
+
+private :
+    void readRegisters();
+    void writeRegister(unsigned char reg, unsigned char val);
+    void writeWordRegister(unsigned char reg, unsigned short val);
+    unsigned long m_compteurErrCom;
+
+    float m_battery_voltage;
+    float m_global_current;
+    float m_current_out1;
+    float m_current_out2;
+
+protected :
+    unsigned char m_address;
+
+    float RegisterToBatteryVoltage(unsigned short val);
+    float RegisterToGlobalCurrent(unsigned short val);
+    float RegisterToCurrentOut1(unsigned short val);
+    float RegisterToCurrentOut2(unsigned short val);
+};
+
+#endif // DSPIC_POWER_ELECTROBOT_BASE_H
