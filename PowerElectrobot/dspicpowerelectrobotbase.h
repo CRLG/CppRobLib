8a278e766724a0ba3d80d56a32c97ddee4694f5b
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2019-11-02 19:55:24 +0100

    #9 [PowerElectrobot] Ajout de la documentation dans le .h

--- PowerElectrobot/dspicpowerelectrobotbase.h
+++ PowerElectrobot/dspicpowerelectrobotbase.h
@@ -1,6 +1,70 @@
 #ifndef DSPIC_POWER_ELECTROBOT_BASE_H
 #define DSPIC_POWER_ELECTROBOT_BASE_H
 
+/*  =============================
+ *    DOCUMENTATION DU DRIVER
+    =============================
+ * Cette classe est le driver de communication avec le dsPIC de la carte PowerElectrobot.
+ * Elle est indépendante de toute plateforme hardware et peut être utilisée sur toute
+ *  plateforme C++ standard.
+ * C'est une classe abstraite pure (ne peut être instanciée directement) et certaines méthodes
+ *  doivent être ré-implémentées par l'applicatif pour faire le lien avec le hardware :
+ *     > writeI2C : envoie d'un buffer de donnée sur le bus I2C de la plateforme.
+ *     > readI2C : lecture d'un certain nombre de données sur le bus I2C de la plateforme.
+ *
+ * Avec cette classe, il est possible d'avoir accès aux mesures effectuées par la carte
+ *     > Mesures de tension batterie : battery_voltage
+ *     > Mesure de courant global : global_current
+ *     > Mesure de courant sur la sortie mesurée n°1 : current_out1
+ *     > Mesure de courant sur la sortie mesurée n°2 : current_out2
+ * Les données "raw" sont les valeurs brutes renvoyées par la carte en [mV] pour la tension et [mA] pour les courants.
+ * L'API est également fournie pour renvoyer directement les valeurs en Volts ou Ampères
+ * La méthode periodicCall() doit être appelée périodiquement par l'applicatif.
+ * Cette méthode va gérer automatiquement les requêtes de lectures des différentes mesures et les convertir dans une unité exploitable.
+
+ * Avec cette classe, il est également possible de piloter les 8 sorties commutables
+ * Soit par l'API sortie par sortie :
+ *      > setOutput(numéro_de_sortie, valeur);
+ * Soit par une valeur globale sur tout le port :
+ *      > setOutputPort(valeur_a_positionner_sur_le_port);
+
+ * La méthode refreshOutputs() peut être appelée périodiquement par l'applicatif pour renvoyer les valeurs attendues sur sorties.
+ * L'objectif est la sécurisation du fonctionnement du système au cas où un transfert se serait mal passé.
+
+ * Attention :
+ * - La liste des registres "T_REG_ADDRESS" est commune avec le code du dsPIC.
+ * Lorsque la définition des registres est changé sur le dsPIC, elle doit également l'être dans ce driver.
+ * Dans le cas contraire, les 2 ne se comprendraient plus.
+ *
+ * - Par défaut, l'EEPROM est protégée en écriture.
+ * Avant toute action ayant pour objectif de changer des paramètres en EEPROM (adresse I2C, calibration, ...),
+ *  il sera nécessaire d'envoyer la requête de dévérouillage de l'EEPROM.
+ * Ce mécanisme étant mis en place pour éviter les écritures involotaires ou par erreur.
+
+* Utilisation :
+ *  1. Créer une nouvelle classe héritant de cette classe et réimplémenter les méthodes virtuelles pures.
+
+ *  2. Mise en oeuvre :
+   PowerElectrobot m_power_electrobot;
+    main()
+    {
+       ...
+       m_power_electrobot.init(0x60); // Configure l'adresse I2C où est vue la carte PowerElectrobot
+
+       m_power_electrobot.setOutput(dsPicPowerElectrobotBase::OUTPUT_STOR1, 1);  // Active la sortie STOR_1
+       m_power_electrobot.setOutputPort(0xAA); Active 1 sortie sur 2 (8, 6, 4, 2)
+       Dans une tâche à 50msec :
+            m_power_electrobot.periodicCall();
+
+       Dans une tâche à 500msec : (facultatif)
+            m_power_electrobot.refreshOutputs();
+
+       unsigned short battery_voltage_mV = getRawBatteryVoltage();
+       float battery_voltage_V = getBatteryVoltage();
+    }
+ *
+*/
+
 class dsPicPowerElectrobotBase
 {
     // Enum commun avec le SW dsPIC
@@ -102,6 +166,7 @@ public:
         OUTPUT_STOR7,
         OUTPUT_STOR8
     }tSwitchOutput;
+// Constantes utilisant les numéros de connecteurs indiqués sur la sérigraphie de la carte PowerElectrobot.
 #define OUTPUT_J11 OUTPUT_STOR1
 #define OUTPUT_J13 OUTPUT_STOR2
 #define OUTPUT_J16 OUTPUT_STOR3
