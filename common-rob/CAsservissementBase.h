d55863bd8ab9422d2ddf3e63734a0a4c2f5491c0
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2020-01-05 12:54:36 +0100

    https://github.com/CRLG/MINIBOT_MBED/issues/1 Les cartos asserv sont spécifiques à chaque robot

--- common-rob/CAsservissementBase.h
+++ common-rob/CAsservissementBase.h
@@ -5,30 +5,6 @@
 #ifndef _ASSERVISSEMENT_BASE_H_
 #define _ASSERVISSEMENT_BASE_H_
 
-#define NBRE_POINTS_CARTO_ERREUR 13
-/*const float ini_conv_erreur_dist_vitesse_cur_x[NBRE_POINTS_CARTO_ERREUR] = {-40, -20, -10, -4, -2, -1, 0, 1, 2, 4, 10, 20, 40};							// [cm]
-const float ini_conv_erreur_dist_vitesse_1_cur[NBRE_POINTS_CARTO_ERREUR] = {-50, -35, -25, -11, -2, -1, 0, 1, 2, 11, 25, 35, 50};						// [cm/s] Carto perfo 0% environ 30cm/s2
-const float ini_conv_erreur_dist_vitesse_2_cur[NBRE_POINTS_CARTO_ERREUR] = {-130, -100, -65, -35, -8, -4, 0, 4, 8, 35, 65, 100, 130};	*/				// [cm/s] Carto perfo 100% environ 190cm/s2
-const float ini_conv_erreur_dist_vitesse_cur_x[NBRE_POINTS_CARTO_ERREUR] = {-40, -20, -10, -4, -2, -1, 0, 1, 2, 4, 10, 20, 40};							// [cm]
-const float ini_conv_erreur_dist_vitesse_1_cur[NBRE_POINTS_CARTO_ERREUR] = {-50, -35, -20, -8, -2, -1, 0, 1, 2, 8, 20, 35, 50};						// [cm/s] Carto perfo 0% environ 30cm/s2
-const float ini_conv_erreur_dist_vitesse_2_cur[NBRE_POINTS_CARTO_ERREUR] = {-130, -100, -55, -25, -8, -4, 0, 4, 8, 25, 55, 100, 130};
-
-const float ini_conv_erreur_angle_vitesse_cur_x[NBRE_POINTS_CARTO_ERREUR] = {-1.6, -0.8, -0.4, -0.2, -0.1, -0.05, 0, 0.05, 0.1, 0.2, 0.4, 0.8, 1.6};	// [rad]
-const float ini_conv_erreur_angle_vitesse_1_cur[NBRE_POINTS_CARTO_ERREUR] = {-4, -2.8, -2, -1.4, -1, -0.5, 0, 0.5, 1, 1.4, 2, 2.8, 4};					// [rad/s] Carto perfo 0% environ 5rad/s2
-const float ini_conv_erreur_angle_vitesse_2_cur[NBRE_POINTS_CARTO_ERREUR] = {-8, -5.65, -4, -2.82, -2, -0.5, 0, 0.5, 2, 2.82, 4, 5.65, 8};				// [rad/s] Carto perfo 100% environ 20rad/s2
-
-//const float ini_facteur_cor_vitesse_avance_cur_x[NBRE_POINTS_CARTO_ERREUR] = {-1.57, -1, -0.785, -0.5, -0.3925, 0, 0.3925, 0.5, 0.785, 1, 1.57};		// [rad]
-//const float ini_facteur_cor_vitesse_avance_cur[NBRE_POINTS_CARTO_ERREUR] = {0, 0.6, 0.8, 0.95, 1, 1, 1, 0.95, 0.8, 0.6, 0};							// [-]
-
-typedef enum {
-    cMODE_MANUEL = 0,
-    cMODE_XY_AUTO,
-    cMODE_XY_AUTO_A,
-    cMODE_XY_AUTO_B,
-    cMODE_DISTANCE_ANGLE,
-    cMODE_XY_TETA
-} tTypeAsservissement;
-
 // -----------------------------
 //! Classe de gestion des options d'exécution passees en ligne de commande
 class CAsservissementBase {
@@ -39,6 +15,25 @@ public :
     static const float DISTANCE_PAR_PAS_CODEUR_D;   // Roue droite
     static const float VOIE_ROBOT;                  // Distance entre les 2 roues [cm]
 #define FACTEUR_CONV_DELTA_DIST_VERS_ANGLE		(1 / VOIE_ROBOT)	// (2 * Pi) / ( 2 * Pi * VOIE_ROBOT) => Resultat en rad
+
+    // Les cartos spécifiques au robot
+#define NBRE_POINTS_CARTO_ERREUR 13
+    static const float ini_conv_erreur_dist_vitesse_cur_x[NBRE_POINTS_CARTO_ERREUR];    // [cm]
+    static const float ini_conv_erreur_dist_vitesse_1_cur[NBRE_POINTS_CARTO_ERREUR];    // [cm/s] Carto perfo 0% environ 30cm/s2
+    static const float ini_conv_erreur_dist_vitesse_2_cur[NBRE_POINTS_CARTO_ERREUR];
+
+    static const float ini_conv_erreur_angle_vitesse_cur_x[NBRE_POINTS_CARTO_ERREUR];   // [rad]
+    static const float ini_conv_erreur_angle_vitesse_1_cur[NBRE_POINTS_CARTO_ERREUR];   // [rad/s] Carto perfo 0% environ 5rad/s2
+    static const float ini_conv_erreur_angle_vitesse_2_cur[NBRE_POINTS_CARTO_ERREUR];   // [rad/s] Carto perfo 100% environ 20rad/s2
+
+    typedef enum {
+        cMODE_MANUEL = 0,
+        cMODE_XY_AUTO,
+        cMODE_XY_AUTO_A,
+        cMODE_XY_AUTO_B,
+        cMODE_DISTANCE_ANGLE,
+        cMODE_XY_TETA
+    } tTypeAsservissement;
 
     float te;  // [s] Temps entre chaque appel de la tache asservissement
     // Les paramètres de l'asservissement
@@ -189,8 +184,6 @@ public :
     virtual void setPosition_XYTeta(float x, float y, float teta);
     virtual void setIndiceSportivite(float idx);
     virtual void setCdeMinCdeMax(int min, int max);
-
-
 
 protected :
     float Calc_carto_cur(float entree_x, float axe_x[], float axe_z[], int nb_point_x);
