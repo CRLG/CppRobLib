495ddcb079e7770047d2e92984ca39e949d4e511
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2020-02-18 09:20:05 +0100

    https://github.com/CRLG/MINIBOT_MBED/issues/1 https://github.com/CRLG/LABOTBOX/issues/49 Découpage Minibot/Grosbot/Simulia Télémètres
    Ajout d'une classe de base pour la gestion des télémètres.

---
+++ common-rob/CTelemetresBase.cpp
@@ -0,0 +1,27 @@
+/*! \file CTelemetresBase.cpp
+	\brief Classe qui contient toute l'application
+*/
+#include "CTelemetresBase.h"
+
+//___________________________________________________________________________
+ /*!
+   \brief Constructeur
+
+   \param --
+   \return --
+*/
+CTelemetresBase::CTelemetresBase()
+{
+}
+
+//___________________________________________________________________________
+ /*!
+   \brief Destructeur
+
+   \param --
+   \return --
+*/
+CTelemetresBase::~CTelemetresBase()
+{
+}
+
---
+++ common-rob/CTelemetresBase.h
@@ -0,0 +1,23 @@
+/*! \file CTelemetresBase.h
+    \brief Classe de base qui contient la gestion des 4 LED MBED
+*/
+
+#ifndef _TELEMETRES_BASE_H
+#define _TELEMETRES_BASE_H
+
+//! Classe de base pour la gestion des télémètres
+class CTelemetresBase
+{
+public :
+    CTelemetresBase();
+    ~CTelemetresBase();
+
+    virtual float getDistanceAVG() = 0;
+    virtual float getDistanceAVD() = 0;
+    virtual float getDistanceARG() = 0;
+    virtual float getDistanceARD() = 0;
+
+};
+#endif
+
+
