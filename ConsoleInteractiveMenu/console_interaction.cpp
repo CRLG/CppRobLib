7b46825d4c6e1f0eda8c05d3a85ab87ab3d9f7e4
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2022-12-21 19:18:47 +0100

    #11 Version initiale du module de gestion de menu interactif en console

---
+++ ConsoleInteractiveMenu/console_interaction.cpp
@@ -0,0 +1,403 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <cstdlib>
+
+#include "console_interaction.h"
+
+//ctor
+ConsoleInteraction::ConsoleInteraction()
+{
+    init_page();
+    m_start_page = nullptr;
+    set_prompt_symbol('>');
+    enable_echo(true);
+    force_restart_current_page_on_char('\t');   //TAB
+    force_start_page_on_char(0x1B);             //ESC
+}
+
+//dtor
+ConsoleInteraction::~ConsoleInteraction()
+{
+}
+
+// ______________________________________________
+/*!
+ * \brief Fonction à appeler sur réception de chaque caractère saisie en console
+ * \param car le caractère reçu
+ * Lorsque la chaine est reçue, elle est passée à receive_str() pour traitement
+ */
+void ConsoleInteraction::receive_car(char car)
+{
+    static char str[STRING_MAX_SIZE];
+    static int i=0;
+
+    if (m_echo) _printf("%c", car); // renvoie le caractère reçu en echo
+
+    if (car=='\n' || car=='\r') {
+        str[i] = '\0';  // fin de chaine
+        receive_str(str);
+        i=0;
+    }
+    else {
+        if (i<STRING_MAX_SIZE) str[i++] = car;
+        else display_error("Internal error : too long line for buffer size");
+    }
+}
+
+// ______________________________________________
+/*!
+ * \brief Traitement à effectuer lorsqu'une chaine complète est reçue (après ENTREE)
+ * \param str la chaine (sans la touche ENTREE)
+ * \return true si le traitement s'est bien passé / false si la chaine n'est pas reconnue pour la page courante
+ */
+bool ConsoleInteraction::receive_str(char str[])
+{
+    bool status = false;
+
+    // Cas d'un touche entrée seule -> ne rien faire
+    if (strlen(str) == 0) {
+        status = true;
+    }
+    // Cas de la touche ESCAPE : revient au menu de démarrage
+    if ( (str[0] == m_start_page_on_char) && (m_start_page_on_char != 0)) {
+        goto_page(m_start_page);
+        status = true;
+    }
+    // Cas "tabulation" -> ré-affiche le menu
+    if ( (str[0] == m_restart_current_page_on_char) && (m_restart_current_page_on_char!=0)) {
+        goto_page(m_current_page.display_page);
+        status = true;
+    }
+    // Cas d'un menu action qui attend une string
+    if (m_current_page.actionFuncString) {
+        status = ((*this).*m_current_page.actionFuncString)(str);
+    }
+    // Cas d'un menu action qui attend un double
+    if (m_current_page.actionFuncDouble) {
+        double val;
+        if (todouble(str, &val)) {
+            status = ((*this).*m_current_page.actionFuncDouble)(val);
+        }
+    }
+    // Cas d'un menu action qui attend un entier
+    if (m_current_page.actionFuncInt) {
+        int val;
+        if (toint(str, &val)) {
+            status = ((*this).*m_current_page.actionFuncInt)(val);
+        }
+    }
+
+    if (!status) {
+        for (int i=0; i<m_current_page.item_nb; i++) {
+            if (str[0] == m_current_page.carToPageOrAction[i].car) {
+                if (m_current_page.carToPageOrAction[i].page)      goto_page(m_current_page.carToPageOrAction[i].page);               // traite les touches qui changent de page
+                if (m_current_page.carToPageOrAction[i].action)    status = ((*this).*m_current_page.carToPageOrAction[i].action)();  // traite les touches qui déclenchent une action immédiate
+                display_prompt();
+                return true;
+            }
+        }
+    }
+
+    // si on arrive ici avec status=false, c'est que la saisie n'a pas été reconnue valide
+    if (!status) display_unexpected_value();
+    display_prompt();
+    return true;
+}
+
+// ______________________________________________
+/*!
+ * \brief Démarre le menu
+ */
+void ConsoleInteraction::start()
+{
+    if (m_start_page)   goto_page(m_start_page);
+    else                display_error("Start page is not defined !");
+    display_prompt();
+}
+
+// ______________________________________________
+/*!
+ * \brief Affiche le titre de la page
+ * \param title le titre à afficher
+ */
+void ConsoleInteraction::display_title(const char title[])
+{
+    _printf("*****************************************\n\r");
+    _printf("\t%s\n\r", title);
+    _printf("*****************************************\n\r");
+}
+
+// ______________________________________________
+/*!
+ * \brief Affiche le message d'erreur de valeur innatendue
+ */
+void ConsoleInteraction::display_unexpected_value()
+{
+    _printf("Unexpected value\n\r");
+}
+
+// ______________________________________________
+/*!
+ * \brief Affiche un message d'erreur
+ * \param msg le message à afficher
+ */
+void ConsoleInteraction::display_error(const char msg[])
+{
+    _printf("Error: %s\n\r", msg);
+}
+
+// ______________________________________________
+/*!
+ * \brief Définit le symbol du prompt
+ */
+void ConsoleInteraction::display_prompt()
+{
+    _printf("%c ", m_prompt);
+}
+
+// ______________________________________________
+/*!
+ * \brief Affiche un message de type "la touche x déclenche l'action"
+ * \param car le caractère déclenchant l'action
+ * \param msg le message à afficher
+ */
+void ConsoleInteraction::display_option(char car, const char msg[])
+{
+    _printf("   %c........ %s\n\r", car, msg);
+}
+
+// ______________________________________________
+/*!
+ * \brief Affiche une action dans la page
+ * \param msg le message à afficher (ex: "Saisir une valeur décimale pour le coef Kp");
+ */
+void ConsoleInteraction::display_action(const char msg[])
+{
+    _printf("   %s\n\r", msg);
+}
+
+// ______________________________________________
+/*!
+ * \brief Envoie la séquence clear screen "cls" par défaut
+ * Réimplémentation possible par la classe héritée
+ */
+void ConsoleInteraction::clear_screen()
+{
+    _printf("\033[2J");         // clean
+    _printf("\033[1;1H");       // cursor top left
+}
+
+// ______________________________________________
+/*!
+ * \brief Ré-implémentation du printf independant de la plateforme
+ * \param format paramètre idem à la fonction printf
+ */
+void ConsoleInteraction::_printf(const char *format, ...)
+{
+    const int BUFF_SIZE = 256; // à ajuster si les lignes font plus de 256 caractère
+    char buff[BUFF_SIZE];
+    va_list args;
+    va_start(args, format);
+    vsnprintf (buff, BUFF_SIZE, format, args);
+    send_to_console(buff);  // appel de la méthode virtuelle pure d'interaction avec le hardware de la plateforme pour envoyer une chaine sur la console
+    va_end(args);
+}
+
+// ______________________________________________
+/*!
+ * \brief Initialise les information de la page courante
+ */
+void ConsoleInteraction::init_page()
+{
+    m_current_page.item_nb = 0;
+    m_current_page.actionFuncString = nullptr;
+    m_current_page.actionFuncDouble = nullptr;
+    m_current_page.actionFuncInt = nullptr;
+}
+
+// ______________________________________________
+/*!
+ * \brief Déclare une page du menu
+ * \param title le titre de la page
+ * \param fct_display la fonction elle même (à mémoriser pour le mécanisme de ré-affichage de la page)
+ */
+void ConsoleInteraction::declare_page(const char title[], ConsoleInteraction::funcPtrDisplay fct_display)
+{
+    m_current_page.display_page = fct_display;
+    display_title(title);
+    init_page();
+}
+// ______________________________________________
+/*!
+ * \brief Déclare une page comme étant la page de démarrage
+ * \param fct_display la fonction décrivant la page
+ */
+void ConsoleInteraction::declare_start_page(ConsoleInteraction::funcPtrDisplay fct_display)
+{
+    m_start_page = fct_display;
+}
+
+// ______________________________________________
+/*!
+ * \brief Déclare un caractère qui déclenche un changement de page du menu
+ * \param car le caractère déclenchant l'action
+ * \param msg le message à présenter à l'utilisateur dans le menu (Ex: "1.......Menu 1.3.4")
+ * \param fct_display la fonction callback à appeler contenant la desciption de la page à pointer si le caractère car est saisit
+ */
+void ConsoleInteraction::declare_option(char car, const char msg[], ConsoleInteraction::funcPtrDisplay fct_display)
+{
+    tCarToPageOrAction cartopage;
+    cartopage.car = car;
+    cartopage.page = fct_display;
+    cartopage.action = nullptr;
+    m_current_page.carToPageOrAction[m_current_page.item_nb++] = cartopage;
+
+    display_option(car, msg);
+}
+
+// ______________________________________________
+/*!
+ * \brief Déclare une action à exécuter lorsqu'une touche particulière est saisie. Ajoute une ligne dans le menu
+ * \param car le caractère déclenchant l'action
+ * \param msg le message à présenter à l'utilisateur dans le menu (Ex: "a.......Lire les paramètres")
+ * \param fct_action la fonction callback à appeler lorsque le caractère car sera saisit
+ */
+void ConsoleInteraction::declare_action(char car, const char msg[], funcPtrExecute fct_action)
+{
+    tCarToPageOrAction cartoaction;
+    cartoaction.car = car;
+    cartoaction.page = nullptr;
+    cartoaction.action = fct_action;
+    m_current_page.carToPageOrAction[m_current_page.item_nb++] = cartoaction;
+
+    display_option(car, msg);
+}
+
+// ______________________________________________
+/*!
+ * \brief Déclare une action où la valeur attendue est une chaine de caractère
+ * \param msg le message à afficher dans le menu
+ * \param fct_action la fonction à appeler avec le paramètre d'entrée string lorsque la valeur chaine est saisie
+ */
+void ConsoleInteraction::declare_action_string(const char msg[], ConsoleInteraction::funcPtrExecuteString fct_action)
+{
+    display_action(msg);
+    m_current_page.actionFuncString = fct_action;
+}
+
+// ______________________________________________
+/*!
+ * \brief Déclare une action où la valeur attendue est un double
+ * \param msg le message à afficher dans le menu
+ * \param fct_action la fonction à appeler avec le paramètre d'entrée double lorsque la valeur double est saisie
+ */
+void ConsoleInteraction::declare_action_double(const char msg[], ConsoleInteraction::funcPtrExecuteDouble fct_action)
+{
+    display_action(msg);
+    m_current_page.actionFuncDouble = fct_action;
+}
+
+// ______________________________________________
+/*!
+ * \brief Déclare une action où la valeur attendue est un entirer
+ * \param msg le message à afficher dans le menu
+ * \param fct_action la fonction à appeler avec le paramètre d'entrée int lorsque la valeur entière est saisie
+ */
+void ConsoleInteraction::declare_action_int(const char msg[], ConsoleInteraction::funcPtrExecuteInt fct_action)
+{
+    display_action(msg);
+    m_current_page.actionFuncInt = fct_action;
+}
+
+// ______________________________________________
+/*!
+ * \brief Va au menu pointé
+ * \param menu la fonction contenant la description du menu
+ */
+void ConsoleInteraction::goto_page(funcPtrDisplay menu)
+{
+    clear_screen();
+    if (menu) ((*this).*menu)();
+    else {
+        _printf("Page is not defined !\n\r");
+    }
+}
+
+// ______________________________________________
+/*!
+ * \brief Vérifie et convertit une chaine en valeur double
+ * \param str la chaine d'entrée
+ * \param out_value la valeur double retournée si la conversion a été possible
+ * \return true si la conversion a été possible / false sinon
+ */
+bool ConsoleInteraction::todouble(char str[], double *out_value)
+{
+    if (strlen(str) == 0) return false;
+    if (!out_value) return false;
+    char *pEnd;
+    double val = strtod(str, &pEnd);
+    if (*pEnd != '\0') return false;
+
+    *out_value = val;
+    return true;
+}
+
+// ______________________________________________
+/*!
+ * \brief Vérifie et convertit une chaine en valeur entière
+ * \param str la chaine d'entrée
+ * \param out_value la valeur entière retournée si la conversion a été possible
+ * \return true si la conversion a été possible / false sinon
+ */
+bool ConsoleInteraction::toint(char str[], int *out_value)
+{
+    if (strlen(str) == 0) return false;
+    if (!out_value) return false;
+    char *pEnd;
+    int val = strtol(str, &pEnd, 10);
+    if (*pEnd != '\0') return false;
+
+    *out_value = val;
+    return true;
+}
+
+// ______________________________________________
+/*!
+ * \brief Change le caractère de prompt
+ * \param prompt le caractère de prompt souhaité
+ */
+void ConsoleInteraction::set_prompt_symbol(char prompt)
+{
+    m_prompt = prompt;
+}
+
+// ______________________________________________
+/*!
+ * \brief Active ou inhibe l'écho de chaque appui réalisé
+ * \param on_off active ou inhibe l'écho
+ */
+void ConsoleInteraction::enable_echo(bool on_off)
+{
+    m_echo = on_off;
+}
+
+// ______________________________________________
+/*!
+ * \brief Force l'affichage de la page de démarrage
+ * \param car caractère qui déclenche la page de démarrage (valeur spéciale 0 pour désactiver la fonction)
+ */
+void ConsoleInteraction::force_start_page_on_char(char car)
+{
+    m_start_page_on_char = car;
+}
+
+// ______________________________________________
+/*!
+ * \brief Force le ré-affichage de la page courante
+ * \param car caractère qui déclenche l'action (valeur spéciale 0 pour désactiver la fonction)
+ */
+void ConsoleInteraction::force_restart_current_page_on_char(char car)
+{
+    m_restart_current_page_on_char = car;
+}
---
+++ ConsoleInteractiveMenu/console_interaction.h
@@ -0,0 +1,293 @@
+#ifndef _CONSOLE_INTERACTION_H_
+#define _CONSOLE_INTERACTION_H_
+/*
+Cette classe permet de mettre en place un menu interractif composé de pages pour les applications en console.
+Elle est indépdendante de la plateforme matérielle sur laquelle est est exécutée.
+
+Il existe 2 types de pages :
+    - Les pages "classiques" permettant d'accèder à une autre page du menu ou déclencher une action simple
+    - Les pages "saisies" dans lesquelles une valeur est attendue (string, int, double) pour déclencher une action (type changement de valeur d'un paramètre, ...)
+      Les pages "saisies" ont les mêmes possiblités que les pages "classiques", à la différence qu'elles attendent la saisie d'une valeur pour déclencher une action spécifique.
+
+Dans chaque page, il est possible :
+    - d'attribuer une touche pour changer de page dans le menu
+    - d'attribuer une touche pour déclencher une action
+
+Le mécanisme de changement de page est automatisé dans la classe de base ConsoleInteraction.
+Les actions sont des fonctions callback appelées (avec ou sans paramètre suivant le cas).
+Les prototypes des callbacks doivent impérativement être respectées car la classe utilise la notion de "pointeurs de fonctions".
+
+Il existe 2 types d'actions :
+    - Les actions déclenchées par l'appui sur une touche de la page (callback sans paramètre d'entrée)
+    - Les actions déclenchées par une valeur attendue (callback avec paramètre d'entrée et paramètre de sortie de type succès/échec)
+
+La classe ConsoleInteraction est virtuelle pure.
+Une seule fonction doit être ré-implémentée (send_to_console(char msg[])) pour faire le lien avec le hardware
+pour envoyer la chaine de caractère sur le port de communication.
+
+L'utilisateur de la classe doit déclarer ses différentes pages et les actions possibles de chaque page.
+Une page est matérialisée par une fonction.
+Dans ces fonctions sont déclarées le contenu de la page et actions à effecuter par des macros DECLARE_PAGE, DECLARE_OPTION, DECLARE_ACTION, ...
+Ces macros sont présentes pour faciliter et simplifier l'écriture des pages.
+
+Parmi toutes les pages, une sera la page de démarrage.
+L'utilisateur doit le préciser dans la classe héritée par la macro DECLARE_START_PAGE
+
+L'utilisateur de la classe instanciée a accès aux méthodes suivantes :
+    set_prompt_symbol(char prompt)                      : Change le symbol du prompt
+    void enable_echo(bool on_off);                      : Active ou inhibe l'écho (renvoie de chaque caractère reçu)
+    void force_start_page_on_char(char car);            : Sélectionne le caractère (=la touche) qui revient à la page de démarrage (par défaut "ESCAPE"). Mettre la valeur numérique 0 pour inhiber la fonction.
+    void force_restart_current_page_on_char(char car);  : Sélectionne le caractère (=la touche) qui ré-affiche la page courante (par défaut "TABULATION"). Mettre la valeur numérique 0 pour inhiber la fonction.
+
+Ce qui n'est pas pris en charge :
+    - Si dans une page, la même touche est affectée à plusieurs actions, la dernière déclarée sera retenue
+
+===========================================================
+Exemple d'utilisation :
+  - Menu à 6 pages avec changement de pages
+  - 2 pages d'écriture de paramètres
+  - Actions de lecture des paramètres
+===========================================================
+   1. Définition du menu par héritage de la classe ConsoleInteraction
+   ------------------------------------------------------------------
+
+class CMenuApp : public ConsoleInteraction
+{
+public:
+    CMenuApp();
+
+    void send_to_console(char msg[]);  // méthode virtuelle pure ré-implémentée
+
+    void page1();
+    void page1_1();
+    void page1_2();
+    void page1_3();
+    void page_set_param_1();
+    void page_set_param_2();
+
+    bool action_set_param1(double val);
+    bool action_read_param1();
+
+    bool action_set_param2(int val);
+    bool action_read_param2();
+
+    bool action_read_params();
+
+    double m_param1;
+    int m_param2;
+};
+
+
+CMenuApp::CMenuApp()
+{
+    enable_echo(false);
+    DECLARE_START_PAGE(CMenuApp::page1)
+}
+
+// ______________________________________________
+void CMenuApp::send_to_console(char msg[])
+{
+    printf("%s", msg);
+}
+
+// ============================================================================
+//                             LES MENUS
+// ============================================================================
+void CMenuApp::page1()
+{
+    DECLARE_PAGE("Menu Page 1", CMenuApp::page1)
+    DECLARE_OPTION('a', "Page 1.1", CMenuApp::page1_1)
+    DECLARE_OPTION('z', "Page 1.2", CMenuApp::page1_2)
+    DECLARE_OPTION('e', "Page 1.3", CMenuApp::page1_3)
+}
+
+void CMenuApp::page1_1()
+{
+    DECLARE_PAGE("Page 1_1", CMenuApp::page1_1)
+    DECLARE_OPTION('q', "Retour en page principale", CMenuApp::page1)
+    DECLARE_OPTION('r', "Vers page 1_2", CMenuApp::page1_2);
+}
+
+void CMenuApp::page1_2()
+{
+    DECLARE_PAGE("Page 1_2", CMenuApp::page1_2)
+    DECLARE_OPTION('s', "Retour en Page1_1", CMenuApp::page1_1)
+    DECLARE_OPTION('p', "Changement du parametre Param1", CMenuApp::page_set_param_1)
+    DECLARE_OPTION('m', "Changement du parametre Param2", CMenuApp::page_set_param_2)
+    DECLARE_ACTION('n', "Lecture Params", CMenuApp::action_read_params)
+    DECLARE_OPTION('q', "Retour en page principale", CMenuApp::page1)
+}
+
+void CMenuApp::page1_3()
+{
+    DECLARE_PAGE("Page 1_3", CMenuApp::page1_3)
+    DECLARE_OPTION('u', "Retour en Page1_1", CMenuApp::page1_1)
+    DECLARE_OPTION('i', "Retour en Page1_2", CMenuApp::page1_2)
+    DECLARE_OPTION('q', "Retour au menu principal", CMenuApp::page1)
+}
+
+void CMenuApp::page_set_param_1()
+{
+    DECLARE_PAGE("Forçage paramètre Param1", CMenuApp::page_set_param_1)
+    DECLARE_ACTION('r', "Lecture du paramètre", CMenuApp::action_read_param1)
+    DECLARE_OPTION('q', "Retour en page principale", CMenuApp::page1)
+    DECLARE_ACTION_DOUBLE("Entrez une valeur pour Param1", CMenuApp::action_set_param1)
+}
+
+void CMenuApp::page_set_param_2()
+{
+    DECLARE_PAGE("Forçage paramètre Param2", CMenuApp::page_set_param_2)
+    DECLARE_ACTION('r', "Lecture du paramètre", CMenuApp::action_read_param2)
+    DECLARE_OPTION('q', "Retour en page principale", CMenuApp::page_set_param_1)
+    DECLARE_ACTION_INT("Entrez une valeur pour Param2", CMenuApp::action_set_param2)
+}
+
+
+// ============================================================================
+//                             LES ACTIONS
+// ============================================================================
+bool CMenuApp::action_set_param1(double val)
+{
+    _printf("Changement de la valeur du paramètre Param1: %f\n\r", val);
+    m_param1 = val;
+    return true;
+}
+
+bool CMenuApp::action_read_param1()
+{
+    _printf("Valeur du Param1: %f\n\r", m_param1);
+    return true;
+}
+
+bool CMenuApp::action_set_param2(int val)
+{
+    _printf("Changement de la valeur du paramètre Param2: %d\n\r", val);
+    m_param2 = val;
+    return true;
+}
+
+bool CMenuApp::action_read_param2()
+{
+    _printf("Valeur du Param2: %d\n\r", m_param2);
+    return true;
+}
+
+bool CMenuApp::action_read_params()
+{
+    _printf("Valeur du Param1: %f\n\r", m_param1);
+    _printf("Valeur du Param2: %d\n\r", m_param2);
+    return true;
+}
+
+   2. Instantiation du menu
+   -------------------------
+CMenuApp menu;
+
+int main()
+{
+    menu.start();
+
+    while(1) {
+        fflush(stdin);
+        char car = fgetc(stdin);
+        menu.receive_car(car);
+    }
+
+    return 0;
+}
+
+*/
+
+
+/*!
+ * \brief The ConsoleInteraction class
+ *
+ */
+class ConsoleInteraction
+{
+public:
+    ConsoleInteraction();
+    virtual ~ConsoleInteraction();
+
+    // API
+    virtual void receive_car(char car);
+    virtual void start();
+
+    void set_prompt_symbol(char prompt);
+    void enable_echo(bool on_off);
+    void force_start_page_on_char(char car);
+    void force_restart_current_page_on_char(char car);
+
+private :
+    virtual bool receive_str(char str[]);
+    virtual void display_title(const char title[]);
+    virtual void display_unexpected_value();
+    virtual void display_prompt();
+    virtual void display_option(char car, const char msg[]);
+    virtual void display_action(const char msg[]);
+    virtual void clear_screen();
+
+    virtual void send_to_console(char msg[]) = 0;   // Fonction à implémenter par la classe héritée pour faire le lien avec le hardware (envoie d'une chaine de caractèrevers la console)
+
+protected :
+    typedef bool (ConsoleInteraction::* funcPtrExecute)();
+    typedef bool (ConsoleInteraction::* funcPtrExecuteString)(char tab[]);
+    typedef bool (ConsoleInteraction::* funcPtrExecuteDouble)(double);
+    typedef bool (ConsoleInteraction::* funcPtrExecuteInt)(int);
+    typedef void (ConsoleInteraction::* funcPtrDisplay)();
+
+private :
+    static const int STRING_MAX_SIZE = 64;          // Taille max d'une chaine de caractère
+    static const int MAX_ITEM_NB_PER_PAGE = 20;     // Nombre max d'item dans une page
+
+    typedef struct {
+        char car;
+        funcPtrDisplay page;
+        funcPtrExecute action;
+    }tCarToPageOrAction;
+
+    typedef struct {
+        funcPtrDisplay          display_page;                       // Fonction d'affichage du menu
+        int                     item_nb;                            // Nombre d'items (changement de page ou action) dans la page courante
+        tCarToPageOrAction      carToPageOrAction[MAX_ITEM_NB_PER_PAGE];  // Correspondance entre touche et un changement de page ou une action
+        funcPtrExecuteString    actionFuncString;                   // Callback avec string en entrée sur saisie utilisateur sur cette page
+        funcPtrExecuteDouble    actionFuncDouble;                   // Callback avec double en entrée sur saisie utilisateur sur cette page
+        funcPtrExecuteInt       actionFuncInt;                      // Callback avec int en entrée sur saisie utilisateur sur cette page
+    }tCurrentPage;
+
+    funcPtrDisplay m_start_page;
+    tCurrentPage m_current_page;
+
+    char m_prompt;
+    bool m_echo;
+    char m_start_page_on_char;
+    char m_restart_current_page_on_char;
+
+    void goto_page(funcPtrDisplay menu);
+    void init_page();
+
+    bool todouble(char str[], double *out_value);
+    bool toint(char str[], int *out_value);
+
+protected :
+    virtual void _printf(const char *format, ...);
+    virtual void display_error(const char msg[]);
+    void declare_page(const char title[], funcPtrDisplay fct_display);
+    void declare_start_page(funcPtrDisplay fct_display);
+    void declare_option(char car, const char msg[], funcPtrDisplay fct_display);
+    void declare_action(char car, const char msg[], funcPtrExecute fct_action);
+    void declare_action_string(const char msg[], funcPtrExecuteString fct_action);
+    void declare_action_double(const char msg[], funcPtrExecuteDouble fct_action);
+    void declare_action_int(const char msg[],funcPtrExecuteInt fct_action);
+
+    // Macro d'aide à la déclaration des pages et des actions
+    #define DECLARE_PAGE(a, b) declare_page(a, (funcPtrDisplay)&b);
+    #define DECLARE_START_PAGE(a) declare_start_page((funcPtrDisplay)&a);
+    #define DECLARE_OPTION(a, b, c) declare_option(a, b, (funcPtrDisplay)&c);
+    #define DECLARE_ACTION(a, b, c) declare_action(a, b, (funcPtrExecute)&c);
+    #define DECLARE_ACTION_STRING(a, b) declare_action_string(a, (funcPtrExecuteString)&b);
+    #define DECLARE_ACTION_DOUBLE(a, b) declare_action_double(a, (funcPtrExecuteDouble)&b);
+    #define DECLARE_ACTION_INT(a, b) declare_action_int(a, (funcPtrExecuteInt)&b);
+};
+
+#endif // _CONSOLE_INTERACTION_H_
