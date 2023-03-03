a74218b957af098c6b5678dc72cf4fc11c11ae81
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2023-03-03 23:42:22 +0100

    #10 [EncoderDecoder] Distinction signé / non signé

--- Communication/DataEncoderDecoder/data_encoder_decoder.cpp
+++ Communication/DataEncoderDecoder/data_encoder_decoder.cpp
@@ -18,6 +18,8 @@ void CDataEncoderDecoder::encode_bit(unsigned char *buff, unsigned char byte_pos
     else        buff[byte_position] &= ~(1 << bit_position);
 }
 
+
+
 //___________________________________________________________________________
  /*!
    \brief Encode une donnée 8 bits dans le buffer
@@ -26,7 +28,7 @@ void CDataEncoderDecoder::encode_bit(unsigned char *buff, unsigned char byte_pos
    \param data : la valeur à encoder
    \return --
 */
-void CDataEncoderDecoder::encode_int8(unsigned char *buff, unsigned char position, unsigned char data)
+void CDataEncoderDecoder::encode_uint8(unsigned char *buff, unsigned char position, unsigned char data)
 {
     buff[position] = data;
 }
@@ -39,7 +41,7 @@ void CDataEncoderDecoder::encode_int8(unsigned char *buff, unsigned char positio
    \param data : la valeur à encoder
    \return --
 */
-void CDataEncoderDecoder::encode_int16(unsigned char *buff, unsigned char position, unsigned short data)
+void CDataEncoderDecoder::encode_uint16(unsigned char *buff, unsigned char position, unsigned short data)
 {
     buff[position]      = (data >> 8)&0xFF;
     buff[position+1]    = (data&0xFF);
@@ -52,7 +54,49 @@ void CDataEncoderDecoder::encode_int16(unsigned char *buff, unsigned char positi
    \param data : la valeur à encoder
    \return --
 */
-void CDataEncoderDecoder::encode_int32(unsigned char *buff, unsigned char position, unsigned long data)
+void CDataEncoderDecoder::encode_uint32(unsigned char *buff, unsigned char position, unsigned long data)
+{
+    buff[position]      = (data >> 24)&0xFF;
+    buff[position+1]    = (data >> 16)&0xFF;
+    buff[position+2]    = (data >> 8)&0xFF;
+    buff[position+3]    = (data&0xFF);
+}
+
+//___________________________________________________________________________
+ /*!
+   \brief Encode une donnée 8 bits dans le buffer
+   \param buff : le buffer de destination
+   \param position : position de la data dans le buffer
+   \param data : la valeur à encoder
+   \return --
+*/
+void CDataEncoderDecoder::encode_int8(unsigned char *buff, unsigned char position, signed char data)
+{
+    buff[position] = data;
+}
+
+//___________________________________________________________________________
+ /*!
+   \brief Encode une donnée 16 bits dans le buffer (MSB first)
+   \param buff : le buffer de destination
+   \param position : position de la data dans le buffer
+   \param data : la valeur à encoder
+   \return --
+*/
+void CDataEncoderDecoder::encode_int16(unsigned char *buff, unsigned char position, signed short data)
+{
+    buff[position]      = (data >> 8)&0xFF;
+    buff[position+1]    = (data&0xFF);
+}
+//___________________________________________________________________________
+ /*!
+   \brief Encode une donnée 32 bits dans le buffer (MSB first)
+   \param buff : le buffer de destination
+   \param position : position de la data dans le buffer
+   \param data : la valeur à encoder
+   \return --
+*/
+void CDataEncoderDecoder::encode_int32(unsigned char *buff, unsigned char position, signed long data)
 {
     buff[position]      = (data >> 24)&0xFF;
     buff[position+1]    = (data >> 16)&0xFF;
@@ -81,7 +125,7 @@ bool CDataEncoderDecoder::decode_bit(unsigned char *buff, unsigned char byte_pos
    \param position : position de la data dans le buffer
    \return la data sur 8 bits
 */
-unsigned char CDataEncoderDecoder::decode_int8(unsigned char *buff, unsigned char position)
+unsigned char CDataEncoderDecoder::decode_uint8(unsigned char *buff, unsigned char position)
 {
     return buff[position];
 }
@@ -93,7 +137,7 @@ unsigned char CDataEncoderDecoder::decode_int8(unsigned char *buff, unsigned cha
    \param position : position de la data dans le buffer
    \return la data sur 16 bits
 */
-unsigned short CDataEncoderDecoder::decode_int16(unsigned char *buff, unsigned char position)
+unsigned short CDataEncoderDecoder::decode_uint16(unsigned char *buff, unsigned char position)
 {
     return ( ( ((unsigned short)(buff[position+1])) & 0xFF) )  |  ( ( ((unsigned short)(buff[position])) & 0xFF) << 8 ) ;
 }
@@ -105,7 +149,7 @@ unsigned short CDataEncoderDecoder::decode_int16(unsigned char *buff, unsigned c
    \param position : position de la data dans le buffer
    \return la data sur 32 bits
 */
-unsigned long CDataEncoderDecoder::decode_int32(unsigned char *buff, unsigned char position)
+unsigned long CDataEncoderDecoder::decode_uint32(unsigned char *buff, unsigned char position)
 {
     return ( ( ((unsigned long)(buff[position+3])) & 0xFF) )  |
             ( ( ((unsigned long)(buff[position+2])) & 0xFF) << 8 ) |
@@ -113,4 +157,41 @@ unsigned long CDataEncoderDecoder::decode_int32(unsigned char *buff, unsigned ch
             ( ( ((unsigned long)(buff[position])) & 0xFF) << 24 );
 }
 
+//___________________________________________________________________________
+ /*!
+   \brief Décode une donnée 8 bits du buffer
+   \param buff : le buffer de destination
+   \param position : position de la data dans le buffer
+   \return la data sur 8 bits
+*/
+signed char CDataEncoderDecoder::decode_int8(unsigned char *buff, unsigned char position)
+{
+    return buff[position];
+}
 
+//___________________________________________________________________________
+ /*!
+   \brief Décode une donnée 16 bits du buffer (MSB first)
+   \param buff : le buffer de destination
+   \param position : position de la data dans le buffer
+   \return la data sur 16 bits
+*/
+signed short CDataEncoderDecoder::decode_int16(unsigned char *buff, unsigned char position)
+{
+    return ( ( ((signed short)(buff[position+1])) & 0xFF) )  |  ( ( ((unsigned short)(buff[position])) & 0xFF) << 8 ) ;
+}
+
+//___________________________________________________________________________
+ /*!
+   \brief Décode une donnée 32 bits du buffer (MSB first)
+   \param buff : le buffer de destination
+   \param position : position de la data dans le buffer
+   \return la data sur 32 bits
+*/
+signed long CDataEncoderDecoder::decode_int32(unsigned char *buff, unsigned char position)
+{
+    return ( ( ((signed long)(buff[position+3])) & 0xFF) )  |
+            ( ( ((signed long)(buff[position+2])) & 0xFF) << 8 ) |
+            ( ( ((signed long)(buff[position+1])) & 0xFF) << 16 ) |
+            ( ( ((signed long)(buff[position])) & 0xFF) << 24 );
+}
--- Communication/DataEncoderDecoder/data_encoder_decoder.h
+++ Communication/DataEncoderDecoder/data_encoder_decoder.h
@@ -13,14 +13,20 @@ class CDataEncoderDecoder
 {
 public :
     static void encode_bit(unsigned char *buff, unsigned char byte_position, unsigned char bit_position, bool data);
-    static void encode_int8(unsigned char *buff, unsigned char position, unsigned char data);
-    static void encode_int16(unsigned char *buff, unsigned char position, unsigned short data);
-    static void encode_int32(unsigned char *buff, unsigned char position, unsigned long data);
+    static void encode_uint8(unsigned char *buff, unsigned char position, unsigned char data);
+    static void encode_uint16(unsigned char *buff, unsigned char position, unsigned short data);
+    static void encode_uint32(unsigned char *buff, unsigned char position, unsigned long data);
+    static void encode_int8(unsigned char *buff, unsigned char position, signed char data);
+    static void encode_int16(unsigned char *buff, unsigned char position, signed short data);
+    static void encode_int32(unsigned char *buff, unsigned char position, signed long data);
 
     static bool decode_bit(unsigned char *buff, unsigned char byte_position, unsigned char bit_position);
-    static unsigned char  decode_int8(unsigned char *buff, unsigned char position);
-    static unsigned short decode_int16(unsigned char *buff, unsigned char position);
-    static unsigned long  decode_int32(unsigned char *buff, unsigned char position);
+    static unsigned char decode_uint8(unsigned char *buff, unsigned char position);
+    static unsigned short decode_uint16(unsigned char *buff, unsigned char position);
+    static unsigned long  decode_uint32(unsigned char *buff, unsigned char position);
+    static signed char  decode_int8(unsigned char *buff, unsigned char position);
+    static signed short decode_int16(unsigned char *buff, unsigned char position);
+    static signed long  decode_int32(unsigned char *buff, unsigned char position);
 };
 
 #endif
