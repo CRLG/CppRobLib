776b6681b64f1f58132abf0fa368f650257711c5
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2026-02-08 09:41:54 +0100

    https://github.com/CRLG/GROSBOT_STM32/issues/1 Déplacement des filtres Lidar dans CppRobLib pour communauté CPU/Labotbox

---
+++ Lidar/lidar_data_filter_example.h
@@ -0,0 +1,17 @@
+#ifndef _LIDAR_DATA_FILTER_EXAMPLE_H_
+#define _LIDAR_DATA_FILTER_EXAMPLE_H_
+
+#include "lidar_data_filter_base.h"
+
+class CLidarDataFilterExample : public CLidarDataFilterBase
+{
+public:
+    CLidarDataFilterExample();
+
+    /*virtual*/void filter(const CLidarData *data_in, CLidarData *data_out) override;
+
+    int m_data_seuil_distance_suppression;
+    int m_data_seuil_distance_bruit_suppression;
+};
+
+#endif // _LIDAR_DATA_FILTER_EXAMPLE_H_
---
+++ Lidar/lidar_data_filter_tracker.h
@@ -0,0 +1,28 @@
+#ifndef _LIDAR_DATA_FILTER_TRACKER_H_
+#define _LIDAR_DATA_FILTER_TRACKER_H_
+
+#include "lidar_data_filter_base.h"
+
+class CLidarDataFilterTracker : public CLidarDataFilterBase
+{
+public:
+    CLidarDataFilterTracker();
+
+    /*virtual*/void filter(const CLidarData *data_in, CLidarData *data_out) override;
+
+    //Paramétrage
+    double m_d_dist_offset;           // mm
+    int m_i_MAX_BLANK;                // discontinuité possible du blob
+    int m_i_MIN_COUNT;                // taille minimum du blob
+    int m_i_MAX_COUNT;                // taille maximum du blob
+    double m_d_MAX_dist;              // zone max de détection
+    double m_d_MIN_dist;              // zone min de détection
+    double m_d_seuil_filtrage_dist;   // pour fusionner les doublons trops proches en distance
+    int m_i_seuil_filtrage_angle;     // pour fusionner les doublons trops proches en angle
+    static const int m_i_MAX_SAMPLES_THRESHOLD=5;
+    double m_d_seuil_gradient;
+    double m_d_seuil_facteur_forme;
+    int m_dot_size;
+};
+
+#endif // _LIDAR_DATA_FILTER_TRACKER_H_
